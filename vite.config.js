5a8218a78b5feb692ec10f7aa4d26ec63197e827
charitha-d <charithareddy705@gmail.com>
2025-06-28 19:28:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,13 @@ export default defineConfig({
 	server: {
 		port: 3000,
 		proxy: {
-			"/api": {
-				target: "https://chat-backend-a4vm.onrender.com",
-			},
-		},
+  "/api": {
+    target: "https://chat-backend-a4vm.onrender.com",
+    changeOrigin: true,
+    timeout: 600000,        // client timeout: 10 minutes
+    proxyTimeout: 600000,   // proxy timeout: 10 minutes
+  },
+},
+
 	},
 });
